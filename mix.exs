e43b6e5e980adb32f4638d6a199d861e54fad19e
Craig S. Cottingham <craig@cottingham.net>
2020-01-05 22:38:49 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,12 +1,21 @@
 defmodule Kontrol.MixProject do
+  @moduledoc false
+
   use Mix.Project
 
   def project do
     [
       app: :kontrol,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      name: "Kontrol",
+      description: description(),
+      source_url: "http://github.com/FiniteMonkeys/kontrol",
+      homepage_url: "http://github.com/FiniteMonkeys/kontrol",
+      package: package(),
+      elixir: "~> 1.9",
+      elixirc_options: [warnings_as_errors: true],
       elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [espec: :test],
       aliases: aliases(),
@@ -14,33 +23,40 @@ defmodule Kontrol.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Kontrol.Application, []}
+      mod: {Kontrol.Application, []},
+      extra_applications: [:logger]
     ]
   end
 
-  # Specifies which paths to compile per environment.
-  # spec/factories
-  defp elixirc_paths(:test), do: ["lib"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp aliases do
+    [
+      credo: ["credo --strict"],
+      test: ["format", "credo --strict", "dialyzer", "espec"]
+    ]
+  end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.0"},
-      {:espec, "~> 1.6", only: :test},
+      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
+      {:espec, "~> 1.8", only: :test},
+      {:ex_doc, "~> 0.21.2", only: [:dev, :test], runtime: false},
+      {:pid_controller, "~> 0.1.2"},
       {:space_ex, "~> 0.8.0"}
     ]
   end
 
-  # See the documentation for `Mix` for more info on aliases.
-  defp aliases do
+  defp description, do: "An exploration of control theory, using Kerbal Space Program."
+
+  defp elixirc_paths(:test), do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
     [
-      credo: ["credo --strict"],
-      test: ["espec"]
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/FiniteMonkeys/kontrol"}
     ]
   end
 end
